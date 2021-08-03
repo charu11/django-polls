e2be7dc5550eef8ff57c506c64fe7eabb9251515
charun umesh <charunumesh@gmail.com>
2021-08-03 17:43:58 +0530

    views and templates added

--- examplesite/polls/views.py
+++ examplesite/polls/views.py
@@ -1,6 +1,35 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+#from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("hello world... you are at the  polls app")
-# Create your views here.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
+    #return render(request, 'polls/index.html, context) this is the shortcut to rendering
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question Does not Exist')
+    return render(request, 'polls/detail.html', {'question': question})
+
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question': question})    
+
+def results(request, question_id):
+    return HttpResponse('you are looking at the results of the question %s.' % question_id)    
+
+
+def vote(request, question_id):
+    return HttpResponse('you are voting on question %s.' % question_id)    
